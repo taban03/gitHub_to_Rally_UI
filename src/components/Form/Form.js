9f9f7380893abe858635be1823635fbb8823f438
at670475 <andrea.tabone@broadcom.com>
2019-12-17 15:08:16 +0100

    Add more rules

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,11 +4,7 @@ import AddIcon from '@material-ui/icons/Add';
 import "./File.css";
 import Fab from '@material-ui/core/Fab';
 import Tooltip from '@material-ui/core/Tooltip';
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
-import ListItemText from '@material-ui/core/ListItemText'
 import TextField from '@material-ui/core/TextField';
-import Menu from '@material-ui/core/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import update from 'react-addons-update';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -43,7 +39,10 @@ export class IncorporationForm extends React.Component {
             name: "",
             message: [],
             isClicked: false,
-            shareholders: [{ name: "", isA: false, isTaskPar: false, and: "text" }]
+            shareholders: [{ name: "", isA: false, isTaskPar: false, and: "text" }],
+            shareholdersMergedPr: [{ name: "", isA: false, isTaskPar: false, and: "text" }],
+            shareholdersOpenPr: [{ name: "", isA: false, isTaskPar: false, and: "text" }],
+            shareholdersCommits: [{ name: "", isA: false, isTaskPar: false, and: "text" }]
         };
 
     }
@@ -60,6 +59,7 @@ export class IncorporationForm extends React.Component {
 
     handleSubmit = evt => {
         const { name, shareholders } = this.state;
+        const { handleAddShareholderForMergePr } = this.state;
         alert(`Incorporated: ${name} with ${shareholders.length} shareholders`);
     };
 
@@ -67,6 +67,16 @@ export class IncorporationForm extends React.Component {
 
         this.setState({
             shareholders: this.state.shareholders.concat([{ name: "", isA: false }])
+        });
+
+        console.log(this.state.isClicked)
+
+    };
+
+    handleAddShareholderForMergePr = () => {
+
+        this.setState({
+            handleAddShareholderForMergePr: this.state.shareholders.concat([{ name: "", isA: false }])
         });
 
         console.log(this.state.isClicked)
@@ -90,6 +100,32 @@ export class IncorporationForm extends React.Component {
 
     }
 
+    handleSelectUsForMergedPr = idx => () => {
+
+        this.setState({
+            shareholdersMergedPr: update(this.state.shareholdersMergedPr, {[idx]: {isA: {$set: true}}})
+
+        });
+
+    }
+
+    handleSelectUsForOpenPr = idx => () => {
+
+        this.setState({
+            shareholdersOpenPr: update(this.state.shareholdersOpenPr, {[idx]: {isA: {$set: true}}})
+
+        });
+
+    }
+    handleSelectUsForCommits = idx => () => {
+
+        this.setState({
+            shareholdersCommits: update(this.state.shareholdersCommits, {[idx]: {isA: {$set: true}}})
+
+        });
+
+    }
+
     handleSelectTask = idx => () => {
         console.log("ciao")
         // if (this.state.shareholders.isA) {
@@ -104,6 +140,51 @@ export class IncorporationForm extends React.Component {
         });
 
     }
+
+    handleSelectTaskForMergedPr = idx => () => {
+        console.log("ciao")
+        // if (this.state.shareholders.isA) {
+        //     this.setState({
+        //         shareholders: update(this.state.shareholders, {[idx]: {isA: {$set: false}}})
+        //
+        //     });
+        // }
+        this.setState({
+            shareholdersMergedPr: update(this.state.shareholdersMergedPr, {[idx]: {isTaskPar: {$set: true}}})
+
+        });
+
+    }
+
+    handleSelectTaskForOpenPr = idx => () => {
+        console.log("ciao")
+        // if (this.state.shareholders.isA) {
+        //     this.setState({
+        //         shareholders: update(this.state.shareholders, {[idx]: {isA: {$set: false}}})
+        //
+        //     });
+        // }
+        this.setState({
+            shareholdersOpenPr: update(this.state.shareholdersOpenPr, {[idx]: {isTaskPar: {$set: true}}})
+
+        });
+
+    }
+
+    handleSelectTaskForCommits = idx => () => {
+        console.log("ciao")
+        // if (this.state.shareholders.isA) {
+        //     this.setState({
+        //         shareholders: update(this.state.shareholders, {[idx]: {isA: {$set: false}}})
+        //
+        //     });
+        // }
+        this.setState({
+            shareholdersCommits: update(this.state.shareholdersCommits, {[idx]: {isTaskPar: {$set: true}}})
+
+        });
+
+    }
     onSelectChange = event => {
         console.log("onSelectChange");
     };
@@ -111,7 +192,6 @@ export class IncorporationForm extends React.Component {
     render() {
         const isClicked  = this.state.isClicked ;
         console.log(this.state.shareholders)
-        // console.log(isClicked)
         return (
             <form onSubmit={this.handleSubmit}>
 
@@ -120,17 +200,18 @@ export class IncorporationForm extends React.Component {
                     <div className="rows">
                     <div className="mainLab">
                     <h2>Labels of pull request</h2>
-                <Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholder}>
-                    <Fab color="primary" >
-                        <AddIcon/>
-                    </Fab>
-                </Tooltip>
+                {/*<Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholder}>*/}
+                {/*    <Fab color="primary" >*/}
+                {/*        <AddIcon/>*/}
+                {/*    </Fab>*/}
+                {/*</Tooltip>*/}
                     </div>
                     </div>
                     {!isClicked && (
                         <div>
                 {this.state.shareholders.map((shareholder, idx) => (
                     <div className="shareholder">
+                        <p>When a label </p>
                         <TextField id="standard-basic" label="PR"
                                    // onChange={this.handleShareholderNameChange(idx)}
                         />
@@ -143,9 +224,8 @@ export class IncorporationForm extends React.Component {
                     {/*    <MenuItem key="{3}" value="10">TA</MenuItem>*/}
                     {/*</Select>*/}
                         <DropdownButton id="dropdown-basic-button" title="Item">
-                            <Dropdown.Item onClick={this.handleSelectUs(idx)} href="#/action-1">US</Dropdown.Item>
-                            <Dropdown.Item onClick={this.handleSelectTask(idx)} href="#/action-2">DE</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">TA</Dropdown.Item>
+                            <Dropdown.Item onClick={this.handleSelectUs(idx)} href="#/action-1">US/DE</Dropdown.Item>
+                            <Dropdown.Item onClick={this.handleSelectTask(idx)} href="#/action-2">TA</Dropdown.Item>
                         </DropdownButton>
                         {shareholder.isA && !shareholder.isTaskPar && (
                             <Par />
@@ -167,10 +247,231 @@ export class IncorporationForm extends React.Component {
                  ))}
                         </div>
                     )}
+
                 <div>
                     {this.state.message}
                 </div>
                 </div>
+                <div className="rows">
+                    <div className="mainLab">
+                        <h2>Merged pull requests</h2>
+                        {/*<Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholderForMergePr}>*/}
+                        {/*    <Fab color="primary" >*/}
+                        {/*        <AddIcon/>*/}
+                        {/*    </Fab>*/}
+                        {/*</Tooltip>*/}
+                    </div>
+                </div>
+                {this.state.shareholdersMergedPr.map((shareholder, idx) => (
+                    <div className="shareholder">
+                        <p>Move the </p>
+                        <div className="uslabel"></div>
+                        {/*    <Select onChange={this.handleSelectUs(idx)} labelId="label" id="simpleMenu" value="20">*/}
+                        {/*    <MenuItem key="{1}" onClick={this.handleSelectUs(idx)} value="10" >US</MenuItem>*/}
+                        {/*        <ListItemText onClick={this.handleSelectUs(idx)}></ListItemText>*/}
+                        {/*    <MenuItem key="{2}" value="10">DE</MenuItem>*/}
+                        {/*    <MenuItem key="{3}" value="10">TA</MenuItem>*/}
+                        {/*</Select>*/}
+                        <DropdownButton id="dropdown-basic-button" title="Item">
+                            <Dropdown.Item onClick={this.handleSelectUsForMergedPr(idx)} href="#/action-1">US/DE</Dropdown.Item>
+                            <Dropdown.Item onClick={this.handleSelectTaskForMergedPr(idx)} href="#/action-2">TA</Dropdown.Item>
+                        </DropdownButton>
+                        {shareholder.isA && !shareholder.isTaskPar && (
+                            <Par />
+                        )}
+                        {shareholder.isTaskPar && !shareholder.isA && (
+                            <ParTask />
+                        )}
+
+
+
+                        <button
+                            type="button"
+                            onClick={this.handleRemoveShareholder(0)}
+                            className="small"
+                        >
+                            -
+                        </button>
+                    </div>
+                ))}
+                <div className="rows">
+                    <div className="mainLab">
+                        <h2>Open pull requests</h2>
+                        {/*<Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholderForMergePr}>*/}
+                        {/*    <Fab color="primary" >*/}
+                        {/*        <AddIcon/>*/}
+                        {/*    </Fab>*/}
+                        {/*</Tooltip>*/}
+                    </div>
+                </div>
+                {this.state.shareholdersOpenPr.map((shareholder, idx) => (
+                    <div className="shareholder">
+                        <p>Move the </p>
+                        <div className="uslabel"></div>
+                        {/*    <Select onChange={this.handleSelectUs(idx)} labelId="label" id="simpleMenu" value="20">*/}
+                        {/*    <MenuItem key="{1}" onClick={this.handleSelectUs(idx)} value="10" >US</MenuItem>*/}
+                        {/*        <ListItemText onClick={this.handleSelectUs(idx)}></ListItemText>*/}
+                        {/*    <MenuItem key="{2}" value="10">DE</MenuItem>*/}
+                        {/*    <MenuItem key="{3}" value="10">TA</MenuItem>*/}
+                        {/*</Select>*/}
+                        <DropdownButton id="dropdown-basic-button" title="Item">
+                            <Dropdown.Item onClick={this.handleSelectUsForOpenPr(idx)} href="#/action-1">US/DE</Dropdown.Item>
+                            <Dropdown.Item onClick={this.handleSelectTaskForOpenPr(idx)} href="#/action-2">TA</Dropdown.Item>
+                        </DropdownButton>
+                        {shareholder.isA && !shareholder.isTaskPar && (
+                            <Par />
+                        )}
+                        {shareholder.isTaskPar && !shareholder.isA && (
+                            <ParTask />
+                        )}
+
+
+
+                        <button
+                            type="button"
+                            onClick={this.handleRemoveShareholder(0)}
+                            className="small"
+                        >
+                            -
+                        </button>
+                    </div>
+                ))}
+                <div className="rows">
+                    <div className="mainLab">
+                        <h2>Commits</h2>
+                        {/*<Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholderForMergePr}>*/}
+                        {/*    <Fab color="primary" >*/}
+                        {/*        <AddIcon/>*/}
+                        {/*    </Fab>*/}
+                        {/*</Tooltip>*/}
+                    </div>
+                </div>
+                {this.state.shareholdersCommits.map((shareholder, idx) => (
+                    <div className="shareholder">
+                        <p>Move the </p>
+                        <div className="uslabel"></div>
+                        {/*    <Select onChange={this.handleSelectUs(idx)} labelId="label" id="simpleMenu" value="20">*/}
+                        {/*    <MenuItem key="{1}" onClick={this.handleSelectUs(idx)} value="10" >US</MenuItem>*/}
+                        {/*        <ListItemText onClick={this.handleSelectUs(idx)}></ListItemText>*/}
+                        {/*    <MenuItem key="{2}" value="10">DE</MenuItem>*/}
+                        {/*    <MenuItem key="{3}" value="10">TA</MenuItem>*/}
+                        {/*</Select>*/}
+                        <DropdownButton id="dropdown-basic-button" title="Item">
+                            <Dropdown.Item onClick={this.handleSelectUsForCommits(idx)} href="#/action-1">US/DE</Dropdown.Item>
+                            <Dropdown.Item onClick={this.handleSelectTaskForCommits(idx)} href="#/action-2">TA</Dropdown.Item>
+                        </DropdownButton>
+                        {shareholder.isA && !shareholder.isTaskPar && (
+                            <Par />
+                        )}
+                        {shareholder.isTaskPar && !shareholder.isA && (
+                            <ParTask />
+                        )}
+
+
+
+                        <button
+                            type="button"
+                            onClick={this.handleRemoveShareholder(0)}
+                            className="small"
+                        >
+                            -
+                        </button>
+                    </div>
+                ))}
+                <div className="rows">
+                    <div className="mainLab">
+                        <h2>New Issue</h2>
+                        {/*<Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholderForMergePr}>*/}
+                        {/*    <Fab color="primary" >*/}
+                        {/*        <AddIcon/>*/}
+                        {/*    </Fab>*/}
+                        {/*</Tooltip>*/}
+                    </div>
+                </div>
+                {this.state.shareholdersCommits.map((shareholder, idx) => (
+                    <div className="shareholder">
+                        <p>Create a new defect only if the issue contains this label </p>
+                        <TextField id="standard-basic" label="Defect Tag"
+                            // onChange={this.handleShareholderNameChange(idx)}
+                        />
+                        <div className="uslabel"></div>
+                        {/*    <Select onChange={this.handleSelectUs(idx)} labelId="label" id="simpleMenu" value="20">*/}
+                        {/*    <MenuItem key="{1}" onClick={this.handleSelectUs(idx)} value="10" >US</MenuItem>*/}
+                        {/*        <ListItemText onClick={this.handleSelectUs(idx)}></ListItemText>*/}
+                        {/*    <MenuItem key="{2}" value="10">DE</MenuItem>*/}
+                        {/*    <MenuItem key="{3}" value="10">TA</MenuItem>*/}
+                        {/*</Select>*/}
+                        <DropdownButton id="dropdown-basic-button" title="Item">
+                            <Dropdown.Item onClick={this.handleSelectUsForCommits(idx)} href="#/action-1">US/DE</Dropdown.Item>
+                            <Dropdown.Item onClick={this.handleSelectTaskForCommits(idx)} href="#/action-2">TA</Dropdown.Item>
+                        </DropdownButton>
+                        {shareholder.isA && !shareholder.isTaskPar && (
+                            <Par />
+                        )}
+                        {shareholder.isTaskPar && !shareholder.isA && (
+                            <ParTask />
+                        )}
+
+
+
+                        <button
+                            type="button"
+                            onClick={this.handleRemoveShareholder(0)}
+                            className="small"
+                        >
+                            -
+                        </button>
+                    </div>
+                ))}
+                <div className="rows">
+                    <div className="mainLab">
+                        <h2>Ready</h2>
+                        {/*<Tooltip title="Add" aria-label="add" onClick={this.handleAddShareholderForMergePr}>*/}
+                        {/*    <Fab color="primary" >*/}
+                        {/*        <AddIcon/>*/}
+                        {/*    </Fab>*/}
+                        {/*</Tooltip>*/}
+                    </div>
+                </div>
+                {this.state.shareholdersCommits.map((shareholder, idx) => (
+                    <div className="shareholder">
+                        <p>When the story state of US is </p>
+                        <TextField id="standard-basic" label="US ID"
+                            // onChange={this.handleShareholderNameChange(idx)}
+                        />
+                        <p> and the task with name </p>
+                        <TextField id="standard-basic" label="Task name "
+                            // onChange={this.handleShareholderNameChange(idx)}
+                        />
+                        <p> is in state </p>
+                        <TextField id="standard-basic" label="Task state "
+                            // onChange={this.handleShareholderNameChange(idx)}
+                        />
+                        <p> then mark it as ready. </p>
+                        {/*    <Select onChange={this.handleSelectUs(idx)} labelId="label" id="simpleMenu" value="20">*/}
+                        {/*    <MenuItem key="{1}" onClick={this.handleSelectUs(idx)} value="10" >US</MenuItem>*/}
+                        {/*        <ListItemText onClick={this.handleSelectUs(idx)}></ListItemText>*/}
+                        {/*    <MenuItem key="{2}" value="10">DE</MenuItem>*/}
+                        {/*    <MenuItem key="{3}" value="10">TA</MenuItem>*/}
+                        {/*</Select>*/}
+                        {shareholder.isA && !shareholder.isTaskPar && (
+                            <Par />
+                        )}
+                        {shareholder.isTaskPar && !shareholder.isA && (
+                            <ParTask />
+                        )}
+
+
+
+                        <button
+                            type="button"
+                            onClick={this.handleRemoveShareholder(0)}
+                            className="small"
+                        >
+                            -
+                        </button>
+                    </div>
+                ))}
                 <button>Submit</button>
             </form>
         );
